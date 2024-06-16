708771b42de1e65027673b5f2e207b31742fefee
irsc <rivero.scastilla@gmail.com>
2024-06-16 14:09:03 +0200

    EVent listeners for buttons

--- src/index.js
+++ src/index.js
@@ -1,8 +1,32 @@
 import fillContent from "./content-main";
+import fillMenu from "./content-menu";
+import fillContact from "./content-contact";
+import fillBook from "./content-book";
 
-/* const content = document.getElementById("content");
-console.log(fillContent());
-content.appendChild(fillContent()); */
+
 fillContent();
+
+const content = document.getElementById("content");
+const home = document.getElementById("home");
+const menu = document.getElementById("menu");
+const contact = document.getElementById("contact");
+const book = document.getElementById("book");
+
+home.addEventListener("click", ()=>{
+    content.innerHTML = "";
+    fillContent();
+});
+menu.addEventListener("click", ()=>{
+    content.innerHTML = "";
+    fillMenu();
+});
+contact.addEventListener("click", ()=>{
+    content.innerHTML = "";
+    fillContact();
+});
+book.addEventListener("click", ()=>{
+    content.innerHTML = "";
+    fillBook();
+})
 
 console.log("Script run");
